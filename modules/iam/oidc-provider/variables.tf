4d7278f08ee537573a2a6f5291c5f0025ce88204
Johan T <joxpg@hotmail.com>
2026-07-29 00:43:22 -0500

    chore: update OIDC thumbprints and broaden GitHub Actions role access permissions

--- modules/iam/oidc-provider/variables.tf
+++ modules/iam/oidc-provider/variables.tf
@@ -19,7 +19,7 @@ variable "client_id_list" {
 variable "thumbprint_list" {
   description = "A list of server certificate thumbprints for the OpenID Connect identity provider's server certificates"
   type        = list(string)
-  default     = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a21d2c5758ad36ec14f96f32e68d2980edd7"]
+  default     = ["227203b5317f3818cab5b5ce596132bf36748c0e", "1b511abead59c6ce207077c0bf0e0043b1382612", "1c58a21d2c5758ad36ec14f96f32e68d2980edd7", "6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 variable "tags" {
